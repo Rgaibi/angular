b1ddc304c3711361c7a9734623e0e78c5d463ec0
karim rgaibi <karimrgaibi@gmail.com>
2025-10-20 19:42:52 +0200

    RxJS of() & from() Operator

--- ANGULAR-OBSERVABLES/src/app/app.component.ts
+++ ANGULAR-OBSERVABLES/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { from, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +11,22 @@ export class AppComponent {
 
   data: any[] = [];
 
-  myObservable = new Observable((observer) => {
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
-    observer.error(new Error('Something went wrong'));
-    observer.next(4);
-    observer.next(5);
-    observer.complete();
-  });
+  array1 = [1, 2, 6, 4, 9];
+  array2 = ['a', 'b', 'd', 'e', 'z'];
+
+  // myObservable = new Observable((observer) => {
+  //   observer.next(1);
+  //   observer.next(2);
+  //   observer.next(3);
+  //   observer.error(new Error('Something went wrong'));
+  //   observer.next(4);
+  //   observer.next(5);
+  //   observer.complete();
+  // });
+
+  // myObservable = of(this.array1, this.array2, 30, 'hello', true);
+     myObservable = from(this.array1)
+
 
   GetAsyncData() {
   //   this.myObservable.subscribe((val: any) => {
