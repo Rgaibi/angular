596630086b47f250547ea36d82a30f8c1915ef09
karim rgaibi <karimrgaibi@gmail.com>
2025-10-20 18:51:20 +0200

    Error & Completion of Observable

--- ANGULAR-OBSERVABLES/src/app/app.component.ts
+++ ANGULAR-OBSERVABLES/src/app/app.component.ts
@@ -15,14 +15,36 @@ export class AppComponent {
     observer.next(1);
     observer.next(2);
     observer.next(3);
+    observer.error(new Error('Something went wrong'));
     observer.next(4);
     observer.next(5);
+    observer.complete();
   });
 
   GetAsyncData() {
-    this.myObservable.subscribe((val: any) => {
-      this.data.push(val)
-    });
+  //   this.myObservable.subscribe((val: any) => {
+  //     this.data.push(val)
+  //   },
+
+  //   (err) => {
+  //     alert(err.message)
+  //   },
+  //   () => {
+  //     alert('All the data is streamed')
+  //   }
+  // );
+    this.myObservable.subscribe({
+      next: (val: any) => {
+        this.data.push(val);
+      },
+      error: (err) => {
+        alert(err.message)
+      },
+      complete: () => {
+        alert('All the data is streamed')   
+      }
+
+    })
   }
 
 }
