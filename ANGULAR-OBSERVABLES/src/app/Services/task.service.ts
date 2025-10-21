6fef23fae67bbbb351e3d25f80c24ac228b414ac
karim rgaibi <karimrgaibi@gmail.com>
2025-10-21 19:20:00 +0200

    Subjects in RxJS

---
+++ ANGULAR-OBSERVABLES/src/app/Services/task.service.ts
@@ -0,0 +1,25 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  constructor() { }
+
+  // CreateTask: EventEmitter<string> = new EventEmitter<string>();
+  CreateTask = new Subject<string>();
+
+  // onCreateTask(value: string) {
+  //   this.CreateTask.emit(value)
+
+  // }
+
+    onCreateTask(value: string) {
+    this.CreateTask.next(value)
+
+  }
+  
+
+  
+}
