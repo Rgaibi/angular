a8bfd059dd44f042adb4333639bd8c4eb1ab5375
karim rgaibi <karimrgaibi@gmail.com>
2025-10-23 23:00:17 +0200

    Retrieving Route Parameter Value

--- ANGULAR-ROUTING/src/app/courses/course/course.component.ts
+++ ANGULAR-ROUTING/src/app/courses/course/course.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 import { Course } from 'src/app/Models/course';
 import { CourseService } from 'src/app/Services/course.service';
 
@@ -8,15 +9,23 @@ import { CourseService } from 'src/app/Services/course.service';
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css']
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent implements OnInit, OnDestroy {
   selectedCourse!: Course;
   courseId!: number;
   courseService: CourseService = inject(CourseService);
   activateRoute: ActivatedRoute = inject(ActivatedRoute);
-
+  paramObs!: Subscription;
   ngOnInit(): void {
     // this.courseId = this.activateRoute.snapshot.params['id'];
-    this.courseId = Number(this.activateRoute.snapshot.paramMap.get('id'));
-    this.selectedCourse = this.courseService.courses.find((course) => {return course.id === this.courseId})!;    
+    // this.courseId = Number(this.activateRoute.snapshot.paramMap.get('id'));
+    this.activateRoute.paramMap.subscribe((data) => {
+      this.courseId = +data.get('id')!;
+      this.selectedCourse = this.courseService.courses.find(course =>  course.id === this.courseId)!
+    }) 
+    
   }
+
+      ngOnDestroy() {
+      this.paramObs.unsubscribe();
+    }
 }
