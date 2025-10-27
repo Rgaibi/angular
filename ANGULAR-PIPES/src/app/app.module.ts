a1fec10500b8ea9a85a48eee89b286b4698be0f2
karim rgaibi <karimrgaibi@gmail.com>
2025-10-27 20:47:56 +0100

    Create Custom Pipes

--- ANGULAR-PIPES/src/app/app.module.ts
+++ ANGULAR-PIPES/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
+import { PercentagePipe } from './pipes/percentage.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent
+    AdminComponent,
+    PercentagePipe
   ],
   imports: [
     BrowserModule,
